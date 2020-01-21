658bfa18663ecb69b203bfa56b0c8b666e5c9d35
Obinna Nnodim <obiknows88@gmail.com>
2020-01-21 15:05:22 -0700

    updated copy on the support page

--- src/pages/support-us.js
+++ src/pages/support-us.js
@@ -1,5 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
+import Helmet from "react-helmet"
+import { withPrefix } from "gatsby"
+
 
 import BackHomeButton from "../components/backhomebutton"
 import Layout from "../components/layout"
@@ -8,34 +10,97 @@ import SEO from "../components/seo"
 const SupportUs = () => (
   <Layout>
     <SEO title="Support Us" />
-    <div style={{color:'white'}}>
+    <Helmet>
+      <script src={withPrefix("yt-platform.js")} />
+    </Helmet>
+    <div style={{color:'white', lineHeight: `1.44rem`}}>
+      {/* TITLE */}
       <h1 style={{fontSize:88, textAlign: 'center', letterSpacing: `0.22rem`, textTransform: 'uppercase'}}>Support Us</h1>
-      <h4>Love what we do?</h4>
-      <h4>Learned from our videos?</h4>
-      <h4>Got a lil' extra on the tax returns?</h4>
-      <h4>If we've ever done anything for you or you just felt like supporting us feel free to do it below</h4>
-      <h4>Love and Light Soul Squad</h4>
-      {/* Shop and support */}
-      
-      {/* Remind people that if they click the links from our Amazon, w/in 24 Hours, we still get our cut. */}
-
-
-      {/* Links to PayPal / Patreon */}
-      <div style={{marginTop: 50, textAlign:'center'}}>
-        <Link to="/" style={{color: 'white', backgroundColor: '#3b7bbf', padding: 10, /*fontFamily: 'Lalezar',*/ fontWeight: 700, textDecoration: 'none'}}>
-          Donate with PayPal
-        </Link>
+      {/* INTRO */}
+      <div style={{textAlign: 'center'}}>
+        <h3>Love watchin our journey?</h3>
+        <h3>Learned anything new from our videos?</h3>
+        <h3>Got a lil’ extra on your tax returns and feelin generous?</h3>
+        <h3>We appreciate your support! Help us stay on the road and free up more time to create meaningful content to share with you in one of the ways below:</h3>
+      </div>
+      {/* DIVIDER */}
+      <hr style={{backgroundColor:'#FEC502', marginLeft: `auto`, marginRight: `auto`, width: `60%`}}/>
+      {/* WAYS TO DONATE */}
+      <div style={{marginLeft: 'auto', marginRight: 'auto', width:`80%`}}>
+        {/* 1. Patreon */}
+        <div style={{marginBottom:40, textAlign: 'center'}}>
+          <h2>Patreon</h2>
+          <h4>Donate $5 or more to us every month and you’ll be rewarded with exclusive perks including monthly live video chats, private Facebook group, recipes, Patreon-only merch and more!</h4>
+          {/* Patreon Button */}
+          <div style={{marginTop: 50, textAlign:'center'}}>
+            <a href="https://www.patreon.com/oursoulagenda" style={{color: 'white', backgroundColor: '#E4705F', padding: 10, fontFamily: 'Lalezar', letterSpacing: 1, textTransform: `uppercase`, textDecoration: 'none'}}>
+              Donate with Patreon
+            </a>
+          </div>
+          
+        </div>
+        {/* 2. Paypal */}
+        <div style={{marginBottom: 40, textAlign: 'center'}}>
+          <h2>PayPal</h2>
+          <h4>
+            The fastest way to show us some love with your wallet. Any and all donations appreciated. P.S. $50 buys us one tank of gas ;)
+          </h4>
+          {/* PayPal Button */}
+          <div style={{marginTop: 50, textAlign:'center'}}>
+            <a href="https://www.paypal.com/donate/?token=NADfNkLjUTbRzBy2ipEtqgwAT623koJhuZYw4eYoT3M1r3jf6pXG7MX7y_xm7yKeyB1Tc0&country.x=US&locale.x=" style={{color: 'white', backgroundColor: '#3b7bbf', padding: 10, fontFamily: 'Lalezar', letterSpacing: 1, textTransform: `uppercase`, textDecoration: 'none'}}>
+              Donate with PayPal
+            </a>
+          </div>
+        </div>
+        {/* Airbnb */}
+        <div style={{marginBottom: 40, textAlign: 'center'}}>
+          <h2>Airbnb</h2>
+          <h4>Free money for the both of us! Use the link below to get $55 off of your first stay and we’ll receive $30 in Airbnb credit.</h4>
+          {/* Airbnb Buttons */}
+          <div style={{marginTop: 50, textAlign:'center'}}>
+            <a href="https://www.airbnb.com/c/aissatoub9?currency=USD" style={{color: 'white', backgroundColor: '#E36868', padding: 10, fontFamily: 'Lalezar', letterSpacing: 1, textTransform: `uppercase`, textDecoration: 'none'}}>
+              Book your first Airbnb stay
+            </a>
+          </div>
+          <div style={{marginTop: 30, textAlign:'center'}}>
+            <a href="www.airbnb.com/r/aissatoub9" style={{color: 'white', backgroundColor: '#E36868', padding: 10, fontFamily: 'Lalezar', letterSpacing: 1, textTransform: `uppercase`, textDecoration: 'none'}}>
+              List your space on Airbnb
+            </a>
+          </div>
+        </div>
+        {/* Amazon */}
+        <div style={{marginBottom: 40, textAlign: 'center'}}>
+          <h2>Amazon</h2>
+          <h4>Buying something off of Amazon soon? <br /> If you click the link below, we’ll automatically receive a percentage of whatever you purchase within the next 24 hours—at no extra cost to you! <br /><br />Feel free to bookmark the link and use it anytime you order from Amazon :)</h4>
+          {/* Airbnb Buttons */}
+          <div style={{marginTop: 50, textAlign:'center'}}>
+            <a href="https://www.amazon.com/ref=as_li_ss_tl?ie=UTF8&linkCode=sl2&tag=oursoulagen0c-20&linkId=57be3aeef7a41212ef03d75849346b38" style={{color: 'white', backgroundColor: '#EC954A', padding: 10, fontFamily: 'Lalezar', letterSpacing: 1, textTransform: `uppercase`, textDecoration: 'none'}}>
+              Shop Amazon Now
+            </a>
+          </div>
+        </div>
+        {/* Cash App */}
+        <div style={{marginBottom: 40, textAlign: 'center'}}>
+          <h2>Cash App</h2>
+          <h4>Got a Cash App, We got that too!</h4>
+          {/* CashApp Button */}
+          <div style={{marginTop: 50, textAlign:'center'}}>
+            <div style={{marginLeft:'auto', marginRight:'auto', color: 'white', backgroundColor: '#74BC6F', padding: 10, fontFamily: 'Lalezar', letterSpacing: 1, textTransform: `uppercase`, textDecoration: 'none', width: `min-content`}}>
+              $payobinna
+            </div>
+          </div>
+        </div>
+        {/* YouTube */}
+        <div style={{marginBottom: 40, textAlign: 'center'}}>
+          <h2>YouTube</h2>
+          <h4>Support us by subscribing to our channel and watching our videos!</h4>
+          {/* YouTube Button */}
+          <div style={{ marginTop:50, textAlign: 'center', padding: `10px`, marginLeft: `auto`, marginRight: `auto`, backgroundColor: "#FEC502", width:'min-content', textAlign: "center", }}>
+            <div className="g-ytsubscribe" data-channelid="UC14DDaQZouxfsTxHhXxF0sg" data-layout="full" data-count="default"></div>
+          </div>
+        </div>
       </div>
 
-      <div>
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-          <input type="hidden" name="cmd" value="_donations" />
-          <input type="hidden" name="business" value="oursoulagenda@gmail.com" />
-          <input type="hidden" name="currency_code" value="USD" />
-          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-          <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-        </form>
-      </div>
 
 
       {/* Go Home Button */}
