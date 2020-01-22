9245b62dd5b0bd3435e13d51cfaf592f66ba2591
Obinna Nnodim <obiknows88@gmail.com>
2020-01-22 08:37:34 -0700

    added a ton of pictures and updated the faves list

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,12 +37,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Our Soul Agenda`,
+        short_name: `Our Soul Agenda`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        // background_color: `#663399`,
+        // theme_color: `#663399`,
+        // display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/our-soul-favicon.png`, // This path is relative to the root of the site.
       },
     },
--- src/pages/our-faves.js
+++ src/pages/our-faves.js
@@ -5,31 +5,101 @@ import BackHomeButton from "../components/backhomebutton"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const OurFaves = () => (
-  <Layout>
-    <SEO title="Our Faves" />
-    <div style={{color:'white'}}>
-      <h1 style={{textAlign: 'center', fontSize: 88, letterSpacing: `0.22rem`, textTransform: 'uppercase'}}>Our Faves</h1>
-      <h3>Here's a few of our favorite things that we use in van life and in real life.</h3>
+// FAV IMG IMPORTS
+import DJICameraPhoto from '../images/our-faves/dji-camera.jpg'
+import GoPro3WayPhoto from '../images/our-faves/gopro3way.jpg'
+import AdobeSuitePhoto from '../images/our-faves/adobe-logo.jpg'
+import AudioTechnicaPhoto from '../images/our-faves/audio-technica.jpg'
+import GoalZeroPhoto from '../images/our-faves/goal-zero-yeti.jpg'
+import SolarPanelPhoto from '../images/our-faves/solar-panels.jpg'
+import HarmanKardonPhoto from '../images/our-faves/harman-kardon.jpg'
+import PioneerDJPhoto from '../images/our-faves/pioneer-dj.jpg'
+import CamperStovePhoto from '../images/our-faves/camper-stove.jpg'
+import ToothbrushPhoto from '../images/our-faves/toothbrush.jpg'
+import ToothpastePhoto from '../images/our-faves/toothpaste.jpg'
+import TongueScraperPhoto from '../images/our-faves/tongue-scraper.jpg'
+import BlackSoapPhoto from '../images/our-faves/black-soap.jpg'
+import SheaButterPhoto from '../images/our-faves/shea-butter.jpg'
+import AlmondOilPhoto from '../images/our-faves/almond-oil.jpg'
+import CashAppPhoto from '../images/our-faves/cash-app.png'
+import NikonCameraPhoto from '../images/our-faves/nikon-d3500.png'
 
-      {/* SERVICE TAB LIST */}
-      <>
-        <FavListContainer>
-          <FavListItem></FavListItem>
-          <FavListItem></FavListItem>
-          <FavListItem></FavListItem>
-          <FavListItem></FavListItem>
-          <FavListItem></FavListItem>
-          <FavListItem></FavListItem>
-        </FavListContainer>
-      </>
-      {/* SERVICE TAB LIST  -- END*/}
+// Format the Data so the Videos are viewable
+var favItems = [
+  // DJI OSMO Pocket
+  {imgSrc: DJICameraPhoto, link:"https://amzn.to/36gMe29", title: 'DJI OSMO Pocket', subtitle: `Our main video camera. Shoots 4K. Doesn't face-track melanin as well as advertised`},
+  // GoPro 3-Way Grip
+  {imgSrc: GoPro3WayPhoto, link: "https://amzn.to/2GaCAU3", title: 'GoPro 3-Way Grip', subtitle: `What we use to stabilize our camera for that lit lit footage`},
+  // Adobe Suite
+  {imgSrc: AdobeSuitePhoto, link: "#", title: 'Adobe Suite', subtitle: `Software we use for video editing, photo editing, and all them lit graphic designs`},
+  // Audio-Technica ATH-M50x
+  {imgSrc: AudioTechnicaPhoto, link: "https://amzn.to/3ay5yLK", title: 'Audio-Technica ATH-M50x', subtitle: `Our favorite headphones, our current pair has lasted 4+ years`},
+  // Goal Zero Yeti 1000
+  {imgSrc: GoalZeroPhoto, link: "https://amzn.to/38wkIPR", title: 'Goal Zero Yeti 1000', subtitle: `How we power our electronics off grid and the easiest way to install solar in your van`},
+  // Renogy 100W Solar Panel
+  {imgSrc: SolarPanelPhoto, link: "https://amzn.to/2Rfywsg", title: 'Renogy 100W Solar Panel', subtitle: `We’ve got two of these strapped to our roof, so we really off-grid`},
+  // Harman Kardon Bluetooth Speaker
+  {imgSrc: HarmanKardonPhoto, link: "https://amzn.to/37jwyfK", title: 'Harman Kardon Bluetooth Speaker', subtitle: `How we jam out in the van. Gotta have that boomin soundsystem`},
+  // Pioneer DDJ-SB3
+  {imgSrc: PioneerDJPhoto, link: "https://amzn.to/2TL4SfO", title: 'Pioneer DDJ-SB3', subtitle: `Aïssatou’s trusted DJ controller. For the ill mixes`},
+  // Coleman Camper Stove
+  {imgSrc: CamperStovePhoto, link: "https://amzn.to/3aquMvv", title: 'Coleman Camper Stove', subtitle: `How we cook all of our delicious meals. Where we be fixin up that flame`},
+  // Bamboo Toothbrushes
+  {imgSrc: ToothbrushPhoto, link: "https://amzn.to/2G9ZcUC", title: 'Bamboo Toothbrushes', subtitle: `Our favorite eco-friendly toothbrushes. Gotta keep the grill shinin.`},
+  // Organic Toothpaste Powder
+  {imgSrc: ToothpastePhoto, link: "https://amzn.to/2Rh0P9m", title: 'Organic Toothpaste Powder', subtitle: `All natural, fluoride-free toothpaste. Healthy toothpaste alternative`},
+  // Copper Tongue Scraper
+  {imgSrc: TongueScraperPhoto, link: "https://amzn.to/37fD9Il", title: 'Copper Tongue Scraper', subtitle: `Our favorite tongue-scraper. Necessary for the early morning breaths.`},
+  // African Black Soap
+  {imgSrc: BlackSoapPhoto, link: "https://amzn.to/2TKMPX7", title: 'African Black Soap', subtitle: `Our clear skin secret straight from our ancestors. We use it as a face and body wash`},
+  // Organic Shea Butter
+  {imgSrc: SheaButterPhoto, link: "https://amzn.to/2sMwztR", title: 'Organic Shea Butter', subtitle: `We use this to moisturize our skin and hair. For that gold glow you kno`},
+  // Organic Almond Oil
+  {imgSrc: AlmondOilPhoto, link: "https://amzn.to/36bIsHf", title: 'Organic Almond Oil', subtitle: `What we us to keep our hair soft, shiny and luscious`},
+  // Cash App
+  {imgSrc: CashAppPhoto, link: "https://cash.app/app/RBGQDBW", title: 'Cash App', subtitle: `How we recieve donations, and invest in Bitcoin and stocks on the road. Plus you get a free custom debit card`},
+  // Nikon D3500
+  {imgSrc: NikonCameraPhoto, link: "https://amzn.to/38x2Ap4", title: 'Nikon D3500', subtitle: `How we take hi-res photos and shoot photoshoots for ourselves and our clients`},
+];
 
-      {/* Go Home Button */}
-      <BackHomeButton/>
-    </div>
-  </Layout>
-)
+
+
+const OurFaves = () => {
+  
+  // Build the fav Items list
+  var favItemsList = favItems.map(item => {
+    return (
+      <FavListItem>
+        <a href={item.link}>
+          <FavListItemImg src={item.imgSrc} />
+        </a>
+        <FavListItemHeader>{item.title}</FavListItemHeader>
+        <FavListItemSubtitle>{item.subtitle}</FavListItemSubtitle>
+      </FavListItem>
+    );
+  })
+
+  return (
+    <Layout>
+      <SEO title="Our Faves" />
+      <div style={{color:'white'}}>
+        <h1 style={{textAlign: 'center', fontSize: 88, letterSpacing: `0.22rem`, textTransform: 'uppercase'}}>Our Faves</h1>
+        <h3>Here's a few of our favorite things that we use in van life and in real life.</h3>
+
+        {/* SERVICE TAB LIST */}
+        <>
+          <FavListContainer>
+            {favItemsList} 
+          </FavListContainer>
+        </>
+        {/* SERVICE TAB LIST  -- END*/}
+
+        {/* Go Home Button */}
+        <BackHomeButton/>
+      </div>
+    </Layout>
+  )
+}
 
 const FavListContainer = styled.div`
   display: grid;
@@ -48,9 +118,21 @@ const FavListContainer = styled.div`
 `
 
 const FavListItem = styled.div`
-  background-color: gold;
+  // background-color: gold;
   width: 100%;
-  height: 100px;
+  height: 350px;
+  text-align: center;
+`
+
+const FavListItemImg = styled.img`
+  height: 75%;
+  margin-bottom: 0.5rem;
+`
+const FavListItemHeader = styled.h3`
+  margin-bottom: 0.25rem;
+`
+const FavListItemSubtitle = styled.h6`
+  margin-bottom: 0.5rem;
 `
 
 export default OurFaves
