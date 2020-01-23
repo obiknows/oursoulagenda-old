acec21d98c4b354e5f96b4cfa90b4fbca2fab9aa
Obinna Nnodim <obiknows88@gmail.com>
2020-01-22 19:49:21 -0700

    changed global h3 settings. About page copy is in and is up

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,10 +8,32 @@ import SEO from "../components/seo"
 const About = () => (
   <Layout>
     <SEO title="About Us" />
-    <div style={{color:'white'}}>
+    <div style={{color:'white', lineHeight: 1.55}}>
       <TitleHeader title="About Us"/>
-      <h3>Our story starts out with love.</h3>
-      <h3 style={{textAlign: 'right'}}>A love that started out as a friendship</h3>
+      <h3>
+        We're Aïssatou and Obinna, two creative nomads documenting our journey towards
+        freedom and financial abundance. Our story together starts at Columbia University,
+        where we met as friends in the Big Apple and fell for each other by senior year.
+      </h3>
+      <h3>
+        3000 miles later, after graduation we moved to Oakland where we lived and worked for
+        non-profits and tech. But we quickly we realized that high-paying salaries was not the end
+        all be all for us and we decided to make a change
+      </h3>
+      <h3>
+        After planning for a few months, in June 2019 we bought & renovated our 70s Camper Van, Soula,
+        sold most of our belongings, and moved into the van and began traveling the US full-time, while
+        documenting our experience online.
+      </h3>
+      <h3>
+        These days, we share our knowledge, expertise and lessons from our unique life in order to empower
+        others with valuable info & items that can help them to prosper on their journey.
+      </h3>
+      <h3>
+        All of our work is motivated by our mission to build community with our people and create generational
+        wealth for ourselves and those around us.
+      </h3>
+
       {/* Go Home Button */}
       <BackHomeButton/>
     </div>
