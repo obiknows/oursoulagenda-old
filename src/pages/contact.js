d16c71640fc1719a850477da92cdd5dbd0953696
Obinna Nnodim <obiknows88@gmail.com>
2020-01-22 19:57:53 -0700

    Updated the contact page as well

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,10 +8,11 @@ import SEO from "../components/seo"
 const Contact = () => (
   <Layout>
     <SEO title="Contact Us" />
-    <div style={{color: 'white'}}>
+    <div style={{color: 'white', textAlign: 'center'}}>
       <TitleHeader title="Contact Us" />
-      <h3>Whats good people.</h3>
-      <h3>If you have an business inquiries or just want to leave us a message please use the </h3>
+      <h3>If you have an business inquiries or just want to leave us a message please use the gold button on the bottom right corner of your screen</h3>
+      <h3>We'll get back to you as soon as we can</h3>
+      <h3>Thanks</h3>
       {/* Go Home Button */}
       <BackHomeButton/>
     </div>
